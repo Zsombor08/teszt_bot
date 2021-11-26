7ec9498db65e2c40fadfcc2a3844c54a68bd24c9
Zsombor08 <74534856+Zsombor08@users.noreply.github.com>
2021-11-26 18:44:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -79,10 +79,6 @@ bot.on("message", async message => {
     //////////////////////////////////////////////////////////
 
 
-    let Tulaj = "904859323094671430"
-    let Altulaj = "904859046505500712"
-    let ArcadeV = "904859035424137256"
-
     if(cmd === `${prefix}kick`){
         message.delete()
         if (message.member.roles.cache.has(Tulaj) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
@@ -197,9 +193,9 @@ bot.on("message", async message => {
     }
 
 
-    let ticket_category_id = "912398852823318599"
-let ticket_role_id = "912396731398914049"
-let support_role_id = "904859035424137256"
+    let ticket_category_id = "913846783774441552"
+let ticket_role_id = "913846918495494244"
+let support_role_id = "913847051345866774"
 
 if(cmd === `${prefix}ticket`) {
     let random_num = Math.floor(Math.random() * 999)
@@ -283,6 +279,11 @@ bot.on(newLocal , function (member) {
     message.roles.add(tag_role)
 });**/
 const tag_role = "911345112540385300"
+bot.on("guildMemberAdd" , function (member) {
+    const ujtag = message.mentions.members.first();
+
+    ujtag.roles.add("734757419263066144")
+})
 /**bot.on("GUILD_MEMBER_ADD", function (member) {
         const channelId = member.guild.systemChannelID;
 
@@ -314,7 +315,7 @@ if (message.channel.id === SUGGESTION_CHANNEL) {
     }).catch(console.error);
     return message.delete();
   }**/
-  const SUGGESTION_CHANNEL = "905510884552638485"
+  const SUGGESTION_CHANNEL = "913845228107104276"
  if(cmd === `${prefix}ötlet`){
      if(message.channel.id === SUGGESTION_CHANNEL){
       message.delete()
@@ -350,167 +351,13 @@ if (message.channel.id === SUGGESTION_CHANNEL) {
         }
         )}else return**/
 
-    const hp1 = "909019781779300413"
-    const hp2 = "9909019718143336458"
-    const hp3 = "909019627298897942"
-
-    if(cmd === `${prefix}hp1`){
-        message.delete()
-    if (message.member.roles.cache.has(Tulaj, Altulaj, ArcadeV) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-        let role_member1 = message.mentions.members.first();
-        if(args[0] && role_member1){
-
-            
-            let HP1Embed = new Discord.MessageEmbed()
-            .setTitle("Hibapont 1")
-            .setColor("GREEN")
-            .setDescription(`${role_member1.user.tag}\n kapott egy hibapontot!`)
-            .setFooter(``)
-         .setTimestamp()
-
-        message.channel.send(HP1Embed);
-
-            role_member1.roles.add(hp1)
-        }
-    } else message.reply("Nincs jogosultságod a parancs használatához!")
-    }
-    if(cmd === `${prefix}hp2`){
-        message.delete()
-        if (message.member.roles.cache.has(Tulaj) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-        let role_member1 = message.mentions.members.first();
-        if(args[0] && role_member1){
-
-            
-            let HP2Embed = new Discord.MessageEmbed()
-            .setTitle("Hibapont 2")
-            .setColor("YELLOW")
-            .setDescription(`${role_member1.user.tag}\n kapott két hibapontot!`)
-            .setFooter(``)
-    .setTimestamp()
-
-        message.channel.send(HP2Embed);
-
-            role_member1.roles.add(hp2)
-        }
-    } else message.reply("Nincs jogosultságod a parancs használatához!")
-    }
-
-    if(cmd === `${prefix}hp3`){
-        message.delete()
-        if (message.member.roles.cache.has(Tulaj, Altulaj, ArcadeV) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-        let role_member1 = message.mentions.members.first();
-        if(args[0] && role_member1){
-
-            
-            let HP3Embed = new Discord.MessageEmbed()
-            .setTitle("Hibapont 3")
-            .setColor("RED")
-            .setDescription(`${role_member1.user.tag}\n kapott három hibapontot ezzel autómatikusan ki lett rúgva!`)
-            .setFooter(``)
-    .setTimestamp()
-
-        message.channel.send(HP3Embed);
-
-            role_member1.roles.add(hp3)
-            role_member1.roles.remove("909541799549362196")
-            role_member1.roles.remove("901890003418906639")
-            role_member1.roles.remove("905118198993682502")
-            role_member1.roles.remove("904858636952674354")
-            role_member1.roles.remove("904858748118524005")
-            role_member1.roles.remove("904858905295872050")
-            role_member1.roles.remove("909074859454234635")
-            role_member1.roles.add("906208857213255720")
-            role_member1.roles.remove(hp3)
-        }
-    } else message.reply("Nincs jogosultságod a parancs használatához!")
-}
-
 if(cmd === `${prefix}stm`){
-    message.channel.send(`Információk:\n - !clear <összeg> \n  - !probaidos/felszolgalo/security <Alany> (Ráadja az alanyra a bizonyos rangot + a Tag rangot, ami jelen esetben az előbb felsoroltak. Ha egy próbaidősre tetetsz a bottal egy felszolgálót akkor a próbaidős rang automatikusan lekerül!) \n - !ban/kick <Alany> <Indok> \n - !ticket(létrehoz egy ticket szobát ahova a vezetőség valamint a ticket generálója lát be) \n - !close <alany> <szoba> (töröl egy bizonyos ticket, szobát, valamint leveszi a Ticket rangot a bizonyos emberről!) \n - !hp1/!hp2/!hp3 <alany> (Ráadja az alanyra a hibapont 1/2/3 rangot, a hibapont 3 rangnál automatikusan ki van rúgva az ember, ebből adódóan a bot autómatikusan le veszi róla a rangokat és ráadja a vendég rangot!) \n - !ötlet <üzenet> (az ötlet szobába a minta szerint lehet írni ötleteket, amelyet a bot átalakít és elküld!) \n - !weather <település>(csak úgy beleírtam; lekéri a település időjárását!)`)
+    message.delete()
+    message.channel.send(`Információk:\n - !clear <összeg> \n - !ban/kick <Alany> <Indok> \n - !ticket(létrehoz egy ticket szobát ahova a vezetőség valamint a ticket generálója lát be) \n - !close <alany> <szoba> (töröl egy bizonyos ticket, szobát, valamint leveszi a Ticket rangot a bizonyos emberről!) \n - !ötlet <üzenet> (az ötlet szobába a minta szerint lehet írni ötleteket, amelyet a bot átalakít és elküld!) \n - !weather <település>(lekéri az adott település időjárását!)`)
 }
 
 if(cmd === `${prefix}info`){
-    message.channel.send(`Információk:\n - !ticket(létrehoz egy ticket szobát ahova a vezetőség valamint a ticket generálója lát be) \n - !ötlet <üzenet> (az ötlet szobába a minta szerint lehet írni ötleteket, amelyet a bot átalakít és elküld!) \n - !weather <település>(csak úgy beleírtam; lekéri a település időjárását, ezt a bot-parancsok szobába nyugodtan lehet használni!)`)
-}
-
-const felszolgalo = "904858748118524005"
-const tag = "901890003418906639"
-
-if(cmd === `${prefix}felszolgalo`){
-message.delete()
-if (message.member.roles.cache.has(Tulaj, Altulaj, ArcadeV) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-let member2 = message.mentions.members.first();
-if(args[0] && member2){
-    member2.roles.add(felszolgalo)
-    member2.roles.add(tag)
-    member2.roles.remove("904858636952674354")
-    member2.roles.remove("904858905295872050")
-
-    let felszembed = new Discord.MessageEmbed()
-    .setTitle("Felszolgáló")
-    .setColor("GREEN")
-    .setDescription(`${member2.user.tag}\n mostantól Felszolgáló!`)
-    .setFooter(``)
-    .setTimestamp()
-
-    message.channel.send(felszembed);
-
-} else message.reply("Valami nem stimmel... Használat: !felszolgalo <alany>!")
-} else message.reply("Nincs jogosultságod a parancs használatához!")
-}
-
-const tag2 = "901890003418906639"
-const security_id = "904858905295872050"
-
-if(cmd === `${prefix}security`){
     message.delete()
-    if (message.member.roles.cache.has(Tulaj, Altulaj, ArcadeV) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-    let member3 = message.mentions.members.first();
-    if(args[0] && member3){
-        member3.roles.add(security_id)
-        member3.roles.add(tag2)
-        member3.roles.remove("904858636952674354")
-    member3.roles.remove("904858748118524005")
-    
-        let secembed = new Discord.MessageEmbed()
-        .setTitle("Biztonsági őr")
-        .setColor("BLUE")
-        .setDescription(`${member3.user.tag}\n mostantól Biztonsági őr!`)
-        .setFooter(``)
-    .setTimestamp()
-    
-        message.channel.send(secembed);
-    
-    } else message.reply("Valami nem stimmel... Használat: !felszolgalo <alany>!")
-    } else message.reply("Nincs jogosultságod a parancs használatához!")
-    }
-
-    const tag3 = "901890003418906639"
-    const probaidos_id = "904858636952674354"
-
-
-    if(cmd === `${prefix}probaidos`){
-        message.delete()
-        if (message.member.roles.cache.has(Tulaj, Altulaj, ArcadeV) || message.member.hasPermission("ADMINISTRATOR" || "BAN_MEMBERS")){
-        let member4 = message.mentions.members.first();
-        if(args[0] && member4){
-            member4.roles.add(probaidos_id)
-            member4.roles.add(tag3)
-            member4.roles.remove("904858748118524005")
-            member4.roles.remove("904858905295872050")
-        
-            let probembed = new Discord.MessageEmbed()
-            .setTitle("Próbaidős")
-            .setColor("ORANGE")
-            .setDescription(`${member4.user.tag}\n mostantól Próbaidős!`)
-            .setFooter(``)
-    .setTimestamp()
-        
-            message.channel.send(probembed);
-        
-        } else message.reply("Valami nem stimmel... Használat: !probaidos <alany>!")
-        } else message.reply("Nincs jogosultságod a parancs használatához!")
-        }
-
-});
+    message.channel.send(`Információk:\n - !ticket(létrehoz egy ticket szobát ahova a supportok valamint a ticket generálója lát be) \n - !ötlet <üzenet> (az ötlet szobába a minta szerint lehet írni ötleteket, amelyet a bot átalakít és elküld!) \n - !weather <település>(csak úgy beleírtam; lekéri a település időjárását, ezt a bot-parancsok szobába nyugodtan lehet használni!)`)
+} })
 bot.login(process.env.BOT_TOKEN);
