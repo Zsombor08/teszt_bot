a74ed3042eb272ad4d5246ddf3fb6d6a743ef8c7
Zsombor08 <74534856+Zsombor08@users.noreply.github.com>
2022-02-10 19:27:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -353,24 +353,6 @@ if (message.channel.id === SUGGESTION_CHANNEL) {
      } 
     }
     
-      const SUGGESTION_CHANNEL = "913845228107104276"
-     if(message.channel.id === SUGGESTION_CHANNEL){
-        if(message.member.roles.cache.has("734757419263066144")){
-      message.delete()
-        const newLocal = args;
-    let embed = new Discord.MessageEmbed()
-    .setAuthor(message.author.username,message.author.avatarURL())
-    .setColor("YELLOW")
-    .setDescription(message.content)
-    .setFooter(``)
-    .setTimestamp()
-    message.channel.send(embed).then(msg => {
-      msg.react('👍').then( r => {
-        msg.react('👎')
-      })
-     })
-     } 
-    }
 
 
 if(cmd === `${prefix}stm`){
